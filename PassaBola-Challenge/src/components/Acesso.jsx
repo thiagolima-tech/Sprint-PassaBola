610cb69712cd38d9f5801fb1554f2b22467007cd
Thiago Lima <thiagolima.age@gmail.com>
2025-09-14 17:28:00 -0300

    style/ responsividade inacabada

--- PassaBola-Challenge/src/components/Acesso.jsx
+++ PassaBola-Challenge/src/components/Acesso.jsx
@@ -1,10 +1,10 @@
 export default function ({ btn1, btn2 }) {
   return (
-    <div className="flex flex-col items-center gap-6">
-      <button className="bg-[#300B74] text-[#FFFFFF] w-[177px] h-[52px] text-[22px] rounded-full">
+    <div className="flex flex-col items-center justify-center gap-6 mt-10 md:flex-row">
+      <button className="bg-[#300B74] text-[#FFFFFF] w-[177px] h-[52px] text-[22px] rounded-full md:w-60 md:h-20 md:text-3xl">
         {btn1}
       </button>
-      <button className="bg-[#300B74] text-[#FFFFFF] w-[177px] h-[52px] text-[22px] rounded-full">
+      <button className="bg-[#300B74] text-[#FFFFFF] w-[177px] h-[52px] text-[22px] rounded-full md:w-60 md:h-20 md:text-3xl">
         {btn2}
       </button>
     </div>
--- PassaBola-Challenge/src/pages/Login.jsx
+++ PassaBola-Challenge/src/pages/Login.jsx
@@ -4,8 +4,10 @@ import Acesso from "../components/Acesso";
 export default function () {
   return (
     <div className="min-h-screen flex items-center justify-center bg-[linear-gradient(to_bottom,#300B74_38%,#5A15DA_100%)]">
-      <div className="flex flex-col p-10 gap-10 mx-4 my-20 bg-[#F9F9F9] rounded-lg">
-        <h1 className="text-[#300B74] text-[64px]">Faça o seu login</h1>
+      <div className="flex flex-col p-10 gap-10 mx-4 my-20 bg-[#F9F9F9] rounded-4xl h-[759px] md:w-6xl md:h-[759px] md:items-center">
+        <h1 className="text-[#300B74] text-[64px] md:mt-10 md:text-[96px]">
+          Faça o seu login
+        </h1>
         <div className="flex flex-col gap-6">
           <InfoUsers campo="Email" type="input" />
           <InfoUsers campo="Senha" type="input" />
