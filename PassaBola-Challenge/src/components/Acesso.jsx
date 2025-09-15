56117f914cd7e92bc2a0df41a2f3d6571b0af708
Laboratório FIAP <labsfiap@fiap.com.br>
2025-09-15 20:31:12 -0300

    fix/ func names

--- PassaBola-Challenge/src/components/Acesso.jsx
+++ PassaBola-Challenge/src/components/Acesso.jsx
@@ -1,4 +1,4 @@
-export default function ({ btn1, btn2 }) {
+export default function Acesso({ btn1, btn2 }) {
   return (
     <div className="font-Raleway flex flex-col items-center justify-center gap-6 mt-10 md:flex-row">
       <button className="bg-[#300B74] text-[#FFFFFF] w-[177px] h-[52px] text-[22px] rounded-full cursor-pointer hover:bg-[#220856] hover:shadow-xl transform hover:scale-105 transition duration-100">
