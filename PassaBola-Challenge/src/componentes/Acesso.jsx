8272963a59bf82747cdab659fe4cd051d646b7ea
Thiago Lima <thiagolima.age@gmail.com>
2025-09-15 22:04:26 -0300

    feat/ cadastro e login

--- PassaBola-Challenge/src/componentes/Acesso.jsx
+++ PassaBola-Challenge/src/componentes/Acesso.jsx
@@ -4,18 +4,18 @@ export default function Acesso({ btn1, route1, btn2, handle }) {
 
   return (
     <div className="font-Raleway flex flex-col items-center justify-center gap-6 mt-10 md:flex-row">
-      <Link
-        to={route1}
-        className="flex items-center justify-center bg-[#300B74] text-white w-[177px] h-[52px] text-[22px] rounded-full cursor-pointer hover:bg-[#220856] hover:shadow-xl transform hover:scale-105 transition duration-100"
-      >
-        {btn1}
-      </Link>
       <button
         onClick={handle}
         className="flex items-center justify-center bg-[#300B74] text-white w-[177px] h-[52px] text-[22px] rounded-full cursor-pointer hover:bg-[#220856] hover:shadow-xl transform hover:scale-105 transition duration-100"
       >
         {btn2}
       </button>
+      <Link
+        to={route1}
+        className="flex items-center justify-center bg-[#300B74] text-white w-[177px] h-[52px] text-[22px] rounded-full cursor-pointer hover:bg-[#220856] hover:shadow-xl transform hover:scale-105 transition duration-100"
+      >
+        {btn1}
+      </Link>
     </div>
   );
 }
