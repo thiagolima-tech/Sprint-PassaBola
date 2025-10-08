c626eebd2b3ed07db4cb86021871e92fc35fb0d0
Vitor Alcantara <vitoralcan5@gmail.com>
2025-10-08 20:06:18 -0300

    fix: arrumei noticias campeonatos

--- src/componentes/componentesCampeonatos/CampeonatosAtuais.jsx
+++ src/componentes/componentesCampeonatos/CampeonatosAtuais.jsx
@@ -1,31 +1,7 @@
 import imagemAnuncioCopa from "../../assets/imagemAnuncioCopa.png";
-import CardNoticia from "../componentesNotícia/CardNoticia.jsx";
-import DocumentosCopa from "./DocumentosCopa.jsx";
 import Titulo from "../Titulo.jsx";
-import { useEffect, useState } from "react";
 
 export default function CampeonatosAtuais() {
-
-  const [news, setNews] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    let alive = true;
-
-    fetch('https://68c98d52ceef5a150f654bb1.mockapi.io/passaBola/v1/news', {
-      method: 'GET'
-    })
-      .then((r) => {
-        if (!r.ok) throw new Error("Falha ao buscar notícias");
-        return r.json();
-      })
-      .then((json) => { if (alive) setNews(json); })
-      .catch((e) => { if (alive) setError(e); })
-      .finally(() => { if (alive) setLoading(false); });
-
-    return () => { alive = false; };
-  }, [])
 
   return (
     <section>
@@ -36,24 +12,6 @@ export default function CampeonatosAtuais() {
           className="m-10 rounded-sm md:w-[70%] cursor-pointer"
           alt="Anuncio Copa Passa a Bola"
         />
-      </div>
-      <DocumentosCopa />
-      <Titulo
-        title="Notícias sobre campeonatos"
-        position="start"
-        color="#300B74"
-      />
-      <div
-        className="flex overflow-x-auto gap-6 mb-2 px-10 no-scroll"
-      >
-        {news.map((n) => (
-          <CardNoticia
-            key={n.id}
-            image={n.image}
-            title={n.title}
-            subtitle={n.subtitle}
-          />
-        ))}
       </div>
     </section>
   );
--- src/pages/Campeonatos.jsx
+++ src/pages/Campeonatos.jsx
@@ -1,7 +1,57 @@
-import CampeonatosAtuais from "../componentes/componentesCampeonatos/CampeonatosAtuais.jsx";
+import { useEffect, useState } from "react";
+import DocumentosCopa from "@/componentes/componentesCampeonatos/DocumentosCopa.jsx";
+import Titulo from "@/componentes/Titulo.jsx";
+import CardNoticia from "@/componentes/componentesNotícia/CardNoticia.jsx";
+import CampeonatosAtuais from "@/componentes/componentesCampeonatos/CampeonatosAtuais.jsx";
 
 export default function Campeonatos() {
+
+  const [news, setNews] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    let alive = true;
+
+    fetch('https://68c98d52ceef5a150f654bb1.mockapi.io/passaBola/v1/news', {
+      method: 'GET'
+    })
+      .then((r) => {
+        if (!r.ok) throw new Error("Falha ao buscar notícias");
+        return r.json();
+      })
+      .then((json) => { if (alive) setNews(json); })
+      .catch((e) => { if (alive) setError(e); })
+      .finally(() => { if (alive) setLoading(false); });
+
+    return () => { alive = false; };
+  }, [])
+
   return (
-    <CampeonatosAtuais />
+    <>
+      <CampeonatosAtuais />
+      <DocumentosCopa />
+      <Titulo
+        title="Notícias sobre campeonatos"
+        position="start"
+        color="#300B74"
+      />
+      <div
+        className="flex overflow-x-auto gap-6 mb-2 px-10
+             snap-x snap-mandatory
+             [-ms-overflow-style:none] [scrollbar-width:none]
+             [&::-webkit-scrollbar]:hidden"
+      >
+        {news.map((n) => (
+          <div key={n.id} className="pl-10 shrink-0 snap-start">
+            <CardNoticia
+              image={n.image}
+              title={n.title}
+              subtitle={n.subtitle}
+            />
+          </div>
+        ))}
+      </div>
+    </>
   );
 }
